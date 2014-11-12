f71f8a623b0a9665a34603b300c450cba5f89ec7
tejasgupta <tejasgupta@users.noreply.github.com>
2014-11-12 22:45:16 +1100

    Update app.js

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ $(document).ready(function(){
 			$("button").click();
 		};
 	});
-		$("button").click(function(){
-			var itemValue = document.getElementById('myNumber');
-			var listItem = itemValue.value;
-			
-			if(!$.trim($('#myNumber').val())){
-				alert('please enter valid item in inputbox');
-			} else if (listItem%15 === 0){
-				$('#fizzbuzz').append(listItem + " fizzBuzz </br/>" );
-				document.getElementById('myNumber').value = '';
-			} else if ( (listItem%5) === 0){
-				$('#fizzbuzz').append(listItem + " buzz </br/>" );
-				document.getElementById('myNumber').value = '';
-			} else if ( (listItem%3) === 0){
-				$('#fizzbuzz').append(listItem + " fizz </br/>" );
-				document.getElementById('myNumber').value = '';
-			} else{
-				$('#fizzbuzz').append(listItem + "</br/>" );
-				document.getElementById('myNumber').value = '';
-			}
-		});
+	$("button").click(function(){
+		var itemValue = document.getElementById('myNumber');
+		var listItem = itemValue.value;
+		
+		if(!$.trim($('#myNumber').val())){
+			alert('please enter valid item in inputbox');
+		} else if (listItem%15 === 0){
+			$('#fizzbuzz').append(listItem + " fizzBuzz </br/>" );
+			document.getElementById('myNumber').value = '';
+		} else if ( (listItem%5) === 0){
+			$('#fizzbuzz').append(listItem + " buzz </br/>" );
+			document.getElementById('myNumber').value = '';
+		} else if ( (listItem%3) === 0){
+			$('#fizzbuzz').append(listItem + " fizz </br/>" );
+			document.getElementById('myNumber').value = '';
+		} else{
+			$('#fizzbuzz').append(listItem + "</br/>" );
+			document.getElementById('myNumber').value = '';
+		}
+	});
 });
