1cd1920881f0fcd0043141f4793061ba7d3f809d
tejasgupta <tejashtml0@gmail.com>
2015-04-10 16:47:15 +1000

    final
    everything is sorted out

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 $(document).ready(function(){
-	$("#myNumber").keyup(function(event){
-		if(event.which == 13){
-			event.preventDefault();
-			$("button").click();
-		};
-	});
-	$("button").click(function(){
+	var fizzbuzz = function() {
 		var itemValue = document.getElementById('myNumber');
 		var listItem = itemValue.value;
 		
 		if(!$.trim($('#myNumber').val())){
 			alert('please enter valid item in inputbox');
-		} else if (listItem%15 === 0){
-			$('#fizzbuzz').append(listItem + " fizzBuzz </br/>" );
-			document.getElementById('myNumber').value = '';
-		} else if ( (listItem%5) === 0){
-			$('#fizzbuzz').append(listItem + " buzz </br/>" );
-			document.getElementById('myNumber').value = '';
-		} else if ( (listItem%3) === 0){
-			$('#fizzbuzz').append(listItem + " fizz </br/>" );
-			document.getElementById('myNumber').value = '';
-		} else{
-			$('#fizzbuzz').append(listItem + "</br/>" );
-			document.getElementById('myNumber').value = '';
-		}
+		} else if (listItem%1 != 0){
+			alert('please enter valid item in inputbox');
+		} else { 
+			for(i = 1; i <= listItem; i++){
+				if ( i%15 === 0){
+					$('#fizzbuzz').append("<li>" + i + " - BURST </li></br/>" );
+					document.getElementById('myNumber').value = '';
+				} else if ( i%5 === 0){
+					$('#fizzbuzz').append("<li>"+ i + " - buzz </li></br/>" );
+					document.getElementById('myNumber').value = '';
+				} else if ( i%3 === 0){
+					$('#fizzbuzz').append("<li>"+ i + " - fizz </li></br/>" );
+					document.getElementById('myNumber').value = '';
+				} else{
+					$('#fizzbuzz').append("<li>"+ i + "</li></br/>" );
+					document.getElementById('myNumber').value = '';
+				};
+			};
+		};
+	};
+	$("#myNumber").keyup(function(event){
+		if(event.which == 13){
+			event.preventDefault();
+			$('#fizzbuzz').html('');
+			fizzbuzz();
+		};
+	});
+	$("button").click(function(event){
+		event.preventDefault();
+		$('#fizzbuzz').html('');
+		fizzbuzz();
 	});
 });
